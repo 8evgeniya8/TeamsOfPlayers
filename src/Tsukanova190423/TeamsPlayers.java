e00c2d5324392e37cc78f53be01a1c3889578963
Tsukanova Evgeniya <tata@gmail.com>
2023-04-19 19:00:56 +0300

    виправлення

--- src/Tsukanova190423/TeamsPlayers.java
+++ src/Tsukanova190423/TeamsPlayers.java
@@ -9,21 +9,81 @@ public class TeamsPlayers {
         String team1 = scanner.next();
 
         System.out.println("Введи кількість фрагів для 5-ти гравців першої команди, після фракції одного гравця, нажми \"Enter\" - щоб перейти до наступного ");
-       int team1Players1 = scanner.nextInt();
-       int team1Players2 = scanner.nextInt();
-       int team1Players3 = scanner.nextInt();
-       int team1Players4 = scanner.nextInt();
-       int team1Players5 = scanner.nextInt();
+       int team1Players1 = 0;
+            if (scanner.hasNextInt()){
+            team1Players1=scanner.nextInt();
+            scanner.nextLine();
+            }else {
+            System.out.println("Помилка. Введіть лише число");
+            }
+       int team1Players2 = 0;
+            if (scanner.hasNextInt()){
+            team1Players2=scanner.nextInt();
+            scanner.nextLine();
+            }else {
+            System.out.println("Помилка. Введіть лише число");
+            }
+       int team1Players3 = 0;
+            if (scanner.hasNextInt()){
+            team1Players3=scanner.nextInt();
+            scanner.nextLine();
+            }else {
+            System.out.println("Помилка. Введіть лише число");
+            }
+       int team1Players4 = 0;
+            if (scanner.hasNextInt()){
+            team1Players4=scanner.nextInt();
+            scanner.nextLine();
+            }else {
+            System.out.println("Помилка. Введіть лише число");
+            }
+       int team1Players5 = 0;
+            if (scanner.hasNextInt()){
+            team1Players5=scanner.nextInt();
+            scanner.nextLine();
+            }else {
+            System.out.println("Помилка. Введіть лише число");
+            }
 
         System.out.println("Введіть назву другої команди гравців");
         String team2 = scanner.next();
 
         System.out.println("Введи кількість фрагів для 5-ти гравців другої команди, після фракції одного гравця, нажми \"Enter\" - щоб перейти до наступного ");
-        int team2Players1 = scanner.nextInt();
-        int team2Players2 = scanner.nextInt();
-        int team2Players3 = scanner.nextInt();
-        int team2Players4 = scanner.nextInt();
-        int team2Players5 = scanner.nextInt();
+        int team2Players1 = 0;
+            if (scanner.hasNextInt()){
+            team2Players1=scanner.nextInt();
+            scanner.nextLine();
+            }else {
+            System.out.println("Помилка. Введіть лише число");
+            }
+        int team2Players2 = 0;
+            if (scanner.hasNextInt()){
+            team2Players2=scanner.nextInt();
+            scanner.nextLine();
+            }else {
+            System.out.println("Помилка. Введіть лише число");
+            }
+        int team2Players3 = 0;
+            if (scanner.hasNextInt()){
+            team2Players3=scanner.nextInt();
+            scanner.nextLine();
+            }else {
+            System.out.println("Помилка. Введіть лише число");
+            }
+        int team2Players4 = 0;
+            if (scanner.hasNextInt()){
+            team2Players4=scanner.nextInt();
+            scanner.nextLine();
+            }else {
+            System.out.println("Помилка. Введіть лише число");
+            }
+        int team2Players5 = 0;
+            if (scanner.hasNextInt()){
+            team2Players5=scanner.nextInt();
+            scanner.nextLine();
+            }else {
+            System.out.println("Помилка. Введіть лише число");
+            }
 
         double team2Result=(team2Players1+team2Players2+team2Players3+team2Players4+team2Players5)/5;
         double team1Result=(team1Players1+team1Players2+team1Players3+team1Players4+team1Players5)/5;
